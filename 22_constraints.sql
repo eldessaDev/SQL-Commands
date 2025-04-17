d6ba7e50cdca389174b8c345624a9009dc03bef4
eldessaDev <kennydrummerk@gmail.com>
2025-04-17 18:07:31 -0400

    create en update complete constraints.sql file

---
+++ 22_constraints.sql
@@ -0,0 +1,6 @@
+-- CONSTRAINTS
+
+    NOT NULL        a column can't contain any null values.
+    UNIQUE          a colum can't contain any duplicate values.
+    PRIMARY KEY     a colum that uniqely identifies each row of data.
+    FOREIGN KEY     a colum which is related to a primary key in another table. 
