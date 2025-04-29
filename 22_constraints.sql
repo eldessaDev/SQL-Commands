d916388508cee8e634e00f46c22ef0c6009220cb
eldessaDev <137922024+eldessaDev@users.noreply.github.com>
2025-04-29 09:53:46 -0400

    final update 22_constraint file

--- 22_constraints.sql
+++ 22_constraints.sql
@@ -4,3 +4,10 @@
     UNIQUE          a colum can't contain any duplicate values.
     PRIMARY KEY     a colum that uniqely identifies each row of data.
     FOREIGN KEY     a colum which is related to a primary key in another table. 
+    CHECK           controls the values that can be inserted into a column
+                    example:
+                        CHECK(rating BETWEEN 0 AND 100)
+    DEFAULT         if no value is inserted into a column, you can set a default value
+                    example:
+                        email DEFAULT 'No Data'    
+    
